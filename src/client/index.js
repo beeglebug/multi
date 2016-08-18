7e2719826721ff22c9a94254c651fbf56c8437a9
Stuart Lee <stuart.lee@drpgroup.com>
2016-08-18 19:28:51 +0100

    adding a renderer

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,11 @@
-import connect from './connect'
 import { INPUT_STATE } from '../common/constants/network'
+import io from 'socket.io-client'
+import checkLatency from './checkLatency'
+import { CHAT, JOIN, LEAVE, BOOT, STATE_UPDATE } from '../common/constants/network'
+import Entity from '../common/Entity'
 import { MOVE_LEFT, MOVE_RIGHT, MOVE_DOWN, MOVE_UP } from '../common/constants/actions'
 import equal from 'array-equal'
+import THREE from 'three'
 
 // get this from some kind of server list
 let server = {
@@ -10,20 +14,96 @@ let server = {
   port: 3000
 }
 
-// do this based on user input
-let socket = connect(server)
+let players = []
+let playersById = {}
 
-import { isDown, bindKeyboard, bindings, ACTIONS } from './input'
+window.playersById = playersById
+
+function makePlayer (data) {
+  var player = Entity.hydrate(data)
+  player.renderable = makeCube(player.position)
+  return player
+}
+
+function makeCube (position) {
+  var geometry = new THREE.BoxGeometry(1, 1, 1)
+  var material = new THREE.MeshBasicMaterial({ color: 0x999999 })
+  var cube = new THREE.Mesh(geometry, material)
+  cube.position.set(position.x, position.y, 0)
+  return cube
+}
+
+function addPlayer (data) {
+  let player = makePlayer(data)
+  players.push(player)
+  playersById[player.id] = player
+}
+
+function removePlayer (id) {
+  let targetPlayer = playersById[id]
+  players = players.filter((player) => {
+    return targetPlayer !== player
+  })
+  delete playersById[id]
+}
+
+const connect = function (server) {
+  let connection = server.host + ':' + server.port
+  let socket = io.connect(connection)
+
+  // receive initial data
+  socket.on(BOOT, (data) => {
+    console.log('boot', data)
+    data.players.forEach((player) => {
+      addPlayer(player)
+    })
+    addPlayer(data.player)
+  })
+
+  // chat message received
+  socket.on(CHAT, (msg) =>
+    console.log('chat', msg)
+  )
+
+  socket.on(JOIN, (player) => {
+    console.log('join', player)
+    addPlayer(player)
+  })
+
+  socket.on(LEAVE, (id) => {
+    console.log('leave', id)
+    removePlayer(id)
+  })
+
+  socket.on(STATE_UPDATE, (state) => {
+    console.log('state', state)
+    let player = playersById[state.id]
+    if (player) {
+      player.position.x = state.x
+      player.position.y = state.y
+    }
+  })
+
+  setInterval(() => {
+    checkLatency(socket)
+  }, 2000)
+
+  return socket
+}
+
+// do this on some kind of player input
+let socket = connect(server);
+
+import { isDown, bindKeyboard, bindings } from './input'
 
 bindKeyboard(document)
 
-let pollInterval = 1000 / 25
+let interval = 1000 / 25
 
 var currentInputState = []
 var previousInputState = []
 
 setInterval(() => {
-
   currentInputState = []
 
   if (isDown(bindings[MOVE_UP])) {
@@ -49,4 +129,14 @@ setInterval(() => {
   previousInputState = currentInputState
 
   // also do it locally
-}, pollInterval)
+}, interval)
+
+var scene = new THREE.Scene()
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+// scene.add(cube)
+scene.background = new THREE.Color(0xDDDDDD)
+camera.position.z = 10
+renderer.render(scene, camera)
