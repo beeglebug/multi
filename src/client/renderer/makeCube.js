1c52dd1b27b10abf33e63d76f01870f2d4a599fe
Stuart Lee <stuart.lee@drpgroup.com>
2016-08-19 16:47:25 +0100

    use phong material

--- src/client/renderer/makeCube.js
+++ src/client/renderer/makeCube.js
@@ -3,10 +3,10 @@ import THREE from 'three'
 const makeCube = (position) => {
   var geometry = new THREE.BoxGeometry(1, 1, 1)
   let color = Math.random() * 0xffffff
-  var material = new THREE.MeshBasicMaterial({ color: color })
-  var cube = new THREE.Mesh(geometry, material)
-  cube.position.set(position.x, position.y, 0)
-  return cube
+  var material = new THREE.MeshPhongMaterial({ color: color })
+  var mesh = new THREE.Mesh(geometry, material)
+  mesh.position.set(position.x, position.y, 0)
+  return mesh
 }
 
 export default makeCube
